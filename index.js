9a1af1b76a908a81b38188fb3610e06375fbeb5c
zeitun-adan <zeitun.adan@student.moringaschool.com>
2022-06-06 19:30:20 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+let currentUser = "zeitun"
+let welcomeMessage = 'Welcome to Flatbook, zeitun!'
+
+let excitedWelcomeMessage ='WELCOME TO FLATBOOK, ZEITUN!'
+let shortGreeting = "Welcome, z!"
